69752f660bbdeb6ffcb88b331030a35474171edc
Liang Ding <d@b3log.org>
2021-06-12 22:08:51 +0800

    :art: 图片居中修正

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/88250/gulu v1.1.73
-	github.com/88250/lute v1.7.3-0.20210610081613-5396aed3245a
+	github.com/88250/lute v1.7.3-0.20210612135852-da7dd83fdfdc
 	github.com/gopherjs/gopherjs v0.0.0-20210603182125-eeedf4a0e899 // indirect
 )
--- parser.go
+++ parser.go
@@ -94,4 +94,12 @@ func fixLegacyData(node *ast.Node, idMap *map[string]bool, needFix *bool) {
 		node.Type = ast.NodeParagraph
 		*needFix = true
 	}
+
+	if ast.NodeImage == node.Type {
+		if "" != node.IALAttr("parent-style") && "" == node.IALAttr("style") {
+			node.SetIALAttr("style", node.IALAttr("parent-style"))
+			node.RemoveIALAttr("parent-style")
+			*needFix = true
+		}
+	}
 }
