241b118827e254e7f067ec71f59f5c11189a0286
Liang Ding <d@b3log.org>
2021-07-11 17:21:17 +0800

    :art: 公式解析问题 https://github.com/siyuan-note/siyuan/issues/2392

--- parser.go
+++ parser.go
@@ -50,9 +50,16 @@ func genTreeByJSON(node *ast.Node, tree *parse.Tree, idMap *map[string]bool, nee
 	node.Properties = nil
 
 	// 历史数据订正
-	if ast.NodeList == node.Type && 1 > len(node.Children) {
-		*needFix = true
-		return // 忽略空列表
+	if ast.NodeList == node.Type {
+		if 1 > len(node.Children) {
+			*needFix = true
+			return // 忽略空列表
+		}
+	} else if ast.NodeMathBlock == node.Type {
+		if 1 > len(node.Children) {
+			*needFix = true
+			return // 忽略空公式
+		}
 	}
 	fixLegacyData(node, idMap, needFix)
 
